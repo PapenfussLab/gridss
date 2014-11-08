f0fd97741e6387e223c62d8fb6a2aaa9375cec36
Daniel Cameron <cameron.d@wehi.edu.au>
2014-11-08 11:56:50 +1100

    Updated filters

--- src/main/r/filter.R
+++ src/main/r/filter.R
@@ -1,6 +1,7 @@
 #source("http://bioconductor.org/biocLite.R")
 #biocLite("VariantAnnotation")
 library(VariantAnnotation)
+#setwd("C:\\dev\\idsv\\src\\main\\r")
 source("libgridss.R")
 
 args<-commandArgs(trailingOnly=TRUE)
@@ -9,20 +10,20 @@ print(file)
 #vcf <- readVcf("C:\\dev\\idsv\\778.vcf", "hg19_random")
 #vcf <- vcf[fixed(vcf)$FILTER == "."] # nrow(vcf[fixed(vcf)$FILTER != "."])
 #vcf <- readVcf("C:\\dev\\CPCG0100.vcf", "hg19_random")
-#setwd("W:\\dream\\results\\")
+
 #file <- "CPCG0100-no-normal-assembly-support.vcf"
 # file <- "W:\\dream\\p23\\gridss.bt2-sorted.bam.bam\\bt2-sorted.bam.bam.vcf"
+# file <- "C:\\dev\\dream\\PCSI0072-novo.vcf"
 vcf <- readVcf(file, "hg19_random")
 df <- gridss.truthdetails.processvcf.vcftodf(vcf)
 df[is.na(df)] <- 0
 df$roiString <- paste0(seqnames(rowData(vcf)), ":", start(ranges(rowData(vcf))) - 2000, "-", start(ranges(rowData(vcf))) + 2000)
-df$A_SC[duplicated(df$variantid)] = df$A_SC[duplicated(df$variantid)] / 2 # Temp hack to fix counts
 
 som <- df[
   (df$A_SCNormal + df$A_RPNormal) / (df$A_SC + df$A_RP) < 0.02 & # less than 2% of support is from the normal # grep "A_SC=0," CPCG0100.vcf | grep "A_RP=0," 
-  df$RCNormal >= 8 & # min coverage in the normal supporting the reference allele
+  df$RCNormal >= 6 & # min coverage in the normal supporting the reference allele
   df$A_SCTumour / (df$A_SCTumour + df$RCTumour) >= 0.20 & # at least 20% support for variant in the tumour data
-  df$A_RM >=2 & # assembly support from both sides
+#  df$A_RM >=2 & # assembly support from both sides
   df$A_SC >= 2 & # found an exact breakpoint on both sides
   df$A_SC + df$A_RP/2 >= 8 & # at least this many read pairs support the variant
   df$A_MQRT >= 40 & # reasonable mapping on both sides
@@ -33,11 +34,7 @@ pp <- som
 roi <- paste(som$roiString, collapse=" ")
 somvcf <- vcf[info(vcf)$EVENT %in% som$EVENT,]
 info(somvcf)$SOMATIC
-writeVcf(somvcf, paste0(file, "-som.vcf"), nchunk = NA)
+#writeVcf(somvcf, paste0(file, "-som.vcf"), nchunk = NA)
+# hack workaround for  VariantAnnotation bug
+writeLines(c(VariantAnnotation:::.makeVcfHeader(somvcf), VariantAnnotation:::.makeVcfMatrix(NULL, somvcf)),  paste0(file, "-som.vcf"))
 
-sessionInfo()
-
-hdr <- VariantAnnotation:::.makeVcfHeader(somvcf)
-data <- VariantAnnotation:::.makeVcfMatrix(somvcf)
-sessionInfo()
-VariantAnnotation_1.8.13
--- src/main/r/plots.R
+++ src/main/r/plots.R
@@ -1,5 +1,13 @@
 library(ggplot2)
-
+pp <- df[
+  (df$A_SCNormal + df$A_RPNormal) / (df$A_SC + df$A_RP) < 0.05 & # less than 5% of support is from the normal # grep "A_SC=0," CPCG0100.vcf | grep "A_RP=0," 
+    df$RCNormal >= 6 & # min coverage in the normal supporting the reference allele
+    df$A_SCTumour / (df$A_SCTumour + df$RCTumour) >= 0.20 & # at least 20% support for variant in the tumour data
+    df$A_RM >=2 & # assembly support from both sides
+#    df$A_SC >= 2 & # found an exact breakpoint on both sides
+    df$A_SC + df$A_RP/2 >= 4 & # at least this many read pairs support the variant
+    df$A_MQRT >= 30 & # reasonable mapping
+    1==1,]
 # local anchor length shouldn't mean much
 ggplot(pp, aes(x=A_BLRM, y=A_BLLM)) + geom_point()
 
@@ -13,16 +21,16 @@ ggplot(pp, aes(x=A_BLRM, y=A_BLLM, color=A_BLRM)) + geom_point()
 + scale_y_log10() + scale_colour_gradientn(colours=rainbow(4)) 
 
 # normal coverage
-ggplot(pp, aes(x=RCNormal + SCECNormal)) + geom_histogram(binwidth=1)
+ggplot(pp, aes(x=RCNormal + SCECNormal)) + geom_histogram()
 #total support
-ggplot(pp, aes(x=A_SC)) + geom_histogram(binwidth=1)
-ggplot(pp, aes(x=A_RP)) + geom_histogram(binwidth=1)
-ggplot(pp, aes(x=PC)) + geom_histogram(binwidth=1)
-ggplot(pp, aes(x=RC)) + geom_histogram(binwidth=1)
-ggplot(pp, aes(x=A_SCCLM)) + geom_histogram(binwidth=1)
-ggplot(pp, aes(x=A_MQRM)) + geom_histogram(binwidth=1)
-ggplot(pp, aes(x=A_MQRT)) + geom_histogram(binwidth=1)
-ggplot(pp, aes(x=A_EC)) + geom_histogram(binwidth=1)
+ggplot(pp, aes(x=A_SC)) + geom_histogram()
+ggplot(pp, aes(x=A_RP)) + geom_histogram()
+ggplot(pp, aes(x=PC)) + geom_histogram()
+ggplot(pp, aes(x=RC)) + geom_histogram()
+ggplot(pp, aes(x=A_SCCLM)) + geom_histogram()
+ggplot(pp, aes(x=A_MQRM)) + geom_histogram()
+ggplot(pp, aes(x=A_MQRT)) + geom_histogram()
+ggplot(pp, aes(x=A_EC)) + geom_histogram()
 ggplot(pp, aes(x=A_SCTumour / (A_SCTumour + RCTumour))) + geom_histogram(binwidth=0.05)
 
 # distribution of tumour variant evidence contribution
@@ -30,4 +38,5 @@ ggplot(df, aes(x=(A_SCTumour + A_RPTumour) / (A_SC + A_RP))) + geom_histogram(bi
 # distribution of variant evidence contribution
 ggplot(df, aes(x=A_SC+A_RP, y=RC+PC, color=(A_SC+A_RP)/(A_SC+A_RP+RC+PC))) + geom_point() + scale_y_log10() + scale_x_log10()
 # normal coverage
-ggplot(df, aes(x=RCNormal + SCECNormal)) + geom_histogram(binwidth=1)
+ggplot(df, aes(x=RCNormal + SCECNormal)) + geom_histogram()
+
