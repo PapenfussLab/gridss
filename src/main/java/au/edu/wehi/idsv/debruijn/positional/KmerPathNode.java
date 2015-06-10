b9cfcfb944c78a0d6af20a197c0f39fc9fb28b35
d-cameron <cameron.d@wehi.edu.au>
2015-06-11 02:52:59 +1000

    Partial implementation of position graph reduction

--- src/main/java/au/edu/wehi/idsv/debruijn/positional/KmerPathNode.java
+++ src/main/java/au/edu/wehi/idsv/debruijn/positional/KmerPathNode.java
@@ -1,9 +1,14 @@
 package au.edu.wehi.idsv.debruijn.positional;
 
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.common.collect.Lists;
+import au.edu.wehi.idsv.debruijn.KmerEncodingHelper;
+import au.edu.wehi.idsv.util.IntervalUtil;
+
 import com.google.common.collect.Ordering;
 import com.google.common.primitives.Ints;
 
@@ -28,45 +33,222 @@ public class KmerPathNode extends KmerNode {
 	public long kmer(int offset) { return kmers.get(offset); }
 	public int startPosition(int offset) { return start + offset; }
 	public int endPosition(int offset) { return end + offset; }
-	public int weight() { return weight; }
+	public int weight() { return totalWeight; }
 	public boolean isReference() { return reference; }
 	public int length() { return kmers.size(); }
-	private final List<Long> kmers;
-	private int weight;
-	private final int start;
-	private final int end;
+	private LongArrayList kmers;
+	private IntArrayList weight;
+	private int totalWeight;
+	private int start;
+	private int end;
 	private final boolean reference;
 	public KmerPathNode(long kmer, int weight, int start, int end, boolean reference) {
-		this.kmers = Lists.newArrayList(kmer);
-		this.weight = weight;
+		this.kmers = new LongArrayList(1);
+		this.kmers.add(kmer);
+		this.weight = new IntArrayList(1);
+		this.weight.add(weight);
+		this.totalWeight = weight;
 		this.start = start;
 		this.end = end;
 		this.reference = reference;
 	}
+	private KmerPathNode(LongArrayList kmer, IntArrayList weight, int totalWeight, int start, int end, boolean reference) {
+		this.kmers = kmer.clone();
+		this.weight = weight.clone();
+		this.totalWeight = totalWeight;
+		this.start = start;
+		this.end = end;
+		this.reference = reference;
+	}
+	private KmerPathNode(LongArrayList kmer, IntArrayList weight, int start, int end, boolean reference) {
+		this(kmer, weight, sumWeights(weight), start, end, reference);
+	}
 	public KmerPathNode(KmerNode node) {
 		this(node.kmer(), node.weight(), node.startPosition(), node.endPosition(), node.isReference());
+	}
+	private static int sumWeights(IntArrayList weight) {
+		int sum = 0;
+		for (int i = 0; i < weight.size(); i++) {
+			sum += weight.getInt(i);
+		}
+		return sum;
 	}
 	public void append(KmerAggregateNode node) {
 		assert(node.startPosition() == startPosition(length() - 1) + 1);
 		assert(node.endPosition() == endPosition(length() - 1) + 1);
 		assert(node.isReference() == isReference());
 		kmers.add(node.kmer());
-		weight += node.weight();
+		weight.add(node.weight());
+		totalWeight += node.weight();
+	}
+	/**
+	 * Indicates that this node has been transformed or deleted
+	 *  
+	 *  Edges 
+	 */
+	public void invalidate() {
+		kmers = null;
+		weight = null;
+	}
+	public boolean isValid() {
+		return kmers != null;
 	}
 	/**
 	 * Edges
 	 */
-	List<KmerPathNode> nextList = new ArrayList<KmerPathNode>(3);
-	List<KmerPathNode> prevList = new ArrayList<KmerPathNode>(3);
+	private ArrayList<KmerPathNode> nextList = new ArrayList<KmerPathNode>(3);
+	private ArrayList<KmerPathNode> prevList = new ArrayList<KmerPathNode>(3);
+	/**
+	 * Successor nodes.
+	 * 
+	 * Note: this method accesses the internal collection directly and is neither copied nor wrapped.
+	 */
 	public List<KmerPathNode> next() {
 		return nextList;
 	}
+	/**
+	 * Precedessor nodes.
+	 * 
+	 * Note: this method accesses the internal collection directly and is neither copied nor wrapped.
+	 */
 	public List<KmerPathNode> prev() {
 		return prevList;
 	}
 	public static void addEdge(KmerPathNode from, KmerPathNode to) {
+		assert(from.isValid());
+		assert(to.isValid());
 		from.nextList.add(to);
 		to.prevList.add(from);
+	}
+	/**
+	 * Splits the given interval into a separate node
+	 * @param firstKmerStart start of interval to split out 
+	 * @param firstKmerEnd end of interval to split out
+	 * @return node defined over the given interval
+	 */
+	public KmerPathNode splitOutInterval(int firstKmerStartPosition, int firstKmerEndPosition) {
+		assert(firstKmerStartPosition >= start);
+		assert(firstKmerEndPosition >= end);
+		KmerPathNode returnNode = this;
+		if (firstKmerStartPosition != returnNode.start) {
+			returnNode = splitAt(firstKmerStartPosition);
+		}
+		if (firstKmerEndPosition != returnNode.end) {
+			splitAt(firstKmerEndPosition);
+		}
+		assert(returnNode.start == firstKmerStartPosition);
+		assert(returnNode.end == firstKmerEndPosition);
+		return returnNode;
+	}
+	/**
+	 * Splits out a the node by creating a new node containing the first given number of bases
+	 * 
+	 * Note: PositionalDeBruijnGraphPathNodeGraphSimplifier relies on the fact that the node
+	 * is truncated at the start, not the end.
+	 * 
+	 * @param length length of node to split out
+	 * @return new predecessor node
+	 */
+	public KmerPathNode splitAtLength(int firstNodeLength) {
+		if (firstNodeLength == 0 || firstNodeLength == length()) return this;
+		assert(firstNodeLength > 0);
+		assert(firstNodeLength < length());
+		// copy our new kmers and weights
+		LongArrayList kmerSecond = new LongArrayList(kmers.subList(firstNodeLength, length()));
+		IntArrayList weightSecond = new IntArrayList(weight.subList(firstNodeLength, length()));
+		// let split own our current arrays
+		this.kmers.removeElements(firstNodeLength, this.kmers.size());
+		this.weight.removeElements(firstNodeLength, this.weight.size());
+		KmerPathNode split = new KmerPathNode(
+				this.kmers,
+				this.weight,
+				start,
+				end,
+				reference);
+		this.kmers = kmerSecond;
+		this.weight = weightSecond;
+		this.totalWeight = this.totalWeight - split.weight();
+		this.start += firstNodeLength;
+		this.end += firstNodeLength;
+		// Update incoming edges to split
+		ArrayList<KmerPathNode> prevEmpty = split.prevList;
+		split.prevList = this.prevList;
+		this.prevList = prevEmpty;
+		for (KmerPathNode n : split.prevList) {
+			replaceFirst(n.nextList, this, split);
+		}
+		addEdge(split, this);
+		return split;
+	}
+	private static void replaceFirst(List<KmerPathNode> list, KmerPathNode existing, KmerPathNode replaceWith) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i) == existing) {
+				list.set(i, replaceWith);
+				return;
+			}
+		}
+		throw new IllegalStateException("Could not replace non-existant element " + existing.toString());
+	}
+	/**
+	 * Splits at the given internal start position
+	 * @param internalStart start position to split at
+	 * @return new node starting at the given position
+	 */
+	private KmerPathNode splitAt(int internalStart) {
+		assert(internalStart > start);
+		assert(internalStart < end);
+		KmerPathNode split = new KmerPathNode(kmers, weight, totalWeight, internalStart, end, reference);
+		this.end = internalStart - 1;
+		
+		ArrayList<KmerPathNode> newNextThis = new ArrayList<KmerPathNode>(nextList.size());
+		ArrayList<KmerPathNode> newNextSplit = new ArrayList<KmerPathNode>(nextList.size());
+		for (KmerPathNode adj : nextList) {
+			if (IntervalUtil.overlapsClosed(adj.startPosition(0) - 1, adj.endPosition(0) - 1, this.start, this.end)) {
+				newNextThis.add(adj);
+			}
+			if (IntervalUtil.overlapsClosed(adj.startPosition(0) - 1, adj.endPosition(0) - 1, split.start, split.end)) {
+				newNextThis.add(adj);
+			}
+		}
+		this.nextList = newNextThis;
+		split.nextList = newNextSplit;
+		
+		ArrayList<KmerPathNode> newPrevThis = new ArrayList<KmerPathNode>(prevList.size());
+		ArrayList<KmerPathNode> newPrevSplit = new ArrayList<KmerPathNode>(prevList.size());
+		for (KmerPathNode adj : prevList) {
+			if (IntervalUtil.overlapsClosed(adj.startPosition(0) + 1, adj.endPosition(0) + 1, this.start, this.end)) {
+				newPrevThis.add(adj);
+			}
+			if (IntervalUtil.overlapsClosed(adj.startPosition(0) + 1, adj.endPosition(0) + 1, split.start, split.end)) {
+				newPrevThis.add(adj);
+			}
+		}
+		this.prevList = newPrevThis;
+		split.prevList = newPrevSplit;
+		return split;
+	}
+	public void santiyCheck(int k, int maxSupportWidth, int maxPathLength) {
+		assert(isValid());
+		assert(start <= end);
+		assert(totalWeight > 0);
+		assert(length() <= maxPathLength);
+		assert(end - start <= maxSupportWidth);
+		assert(kmers.size() == length());
+		assert(weight.size() == length());
+		for (int i = 1; i < length(); i++) {
+			assert(KmerEncodingHelper.isNext(k, kmers.getLong(i - 1), kmers.getLong(i)));
+		}
+		assert(sumWeights(weight) == totalWeight);
+		for (KmerPathNode next : nextList) {
+			assert(KmerEncodingHelper.isNext(k, kmer(), next.kmer(0)));
+			assert(IntervalUtil.overlapsClosed(startPosition() + 1, endPosition() + 1, next.startPosition(0), next.endPosition(0)));
+			assert(next.isValid());
+		}
+		for (KmerPathNode prev : prevList) {
+			assert(KmerEncodingHelper.isNext(k, prev.kmer(), kmer(0)));
+			assert(IntervalUtil.overlapsClosed(startPosition(0) - 1, endPosition(0) - 1, prev.startPosition(), prev.endPosition()));
+			assert(prev.isValid());
+		}
 	}
 	@Override
 	public String toString() {
@@ -78,10 +260,10 @@ public class KmerPathNode extends KmerNode {
 			return Ints.compare(left.startPosition(0), right.startPosition(0));
 		}
 	};
-	public static final Ordering<KmerPathNode> ByLastKmerEndPosition = new Ordering<KmerPathNode>() {
+	public static final Ordering<KmerPathNode> ByFirstKmerEndPosition = new Ordering<KmerPathNode>() {
 		@Override
 		public int compare(KmerPathNode left, KmerPathNode right) {
-			return Ints.compare(left.startPosition(), right.startPosition());
+			return Ints.compare(left.startPosition(0), right.startPosition(0));
 		}
 	};
 }
--- src/main/java/au/edu/wehi/idsv/debruijn/positional/PositionalDeBruijnGraphPathNodeGraphSimplifier.java
+++ src/main/java/au/edu/wehi/idsv/debruijn/positional/PositionalDeBruijnGraphPathNodeGraphSimplifier.java
@@ -1,77 +1,296 @@
 package au.edu.wehi.idsv.debruijn.positional;
 
-import java.util.ArrayDeque;
-import java.util.HashSet;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+
 import java.util.Iterator;
 import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
 
 import com.google.common.collect.Iterators;
+import com.google.common.collect.Ordering;
 import com.google.common.collect.PeekingIterator;
+import com.google.common.primitives.Ints;
 
+/**
+ * Graph simplifier that merging similar paths
+ * 
+ * Input: KmerPathNodes in ascending order of start position of the first kmer
+ * 
+ * Output: KmerPathNodes in ascending order of start position of the first kmer
+ * after graph simplification. Each output node is guaranteed to not be modified
+ * by further graph reduction although adjacent node may be modified causing a
+ * change in the node edge list after emission.
+ *  
+ * All input nodes are guaranteed to have all edges defined,
+ * but this is not transitive: nodes that have not been returned by the input
+ * are not guaranteed to either a) have all edges defined, or b) be of full length
+ *  
+ * When collapsing 2 path together, all nodes along both paths must be fully defined
+ * so the edge list of adjacent nodes can be updated if the node is split.
+ * 
+ * Let G(pos) be the graph of all nodes such that first_kmer_start_position(n) <= pos
+ * 
+ * For branch collapse:
+ * ... -* - * <- root node we traverse backwards looking for similar paths
+ *        /
+ * ...   *
+ * 
+ * Let n be a node such that next(n) = root
+ * To collapse a path onto n, n needs to be split along:
+ * a) length: alternate path may have nodes of shorter length,
+ *     this requires this node to be split into shorter nodes
+ * b) start/end: the alternate path may have a smaller positional window of validity,
+ *     requiring splitting into multiple validity intervals
+ *
+ * If we collapse a branch as soon as pos >= first_kmer_end_position of root then:
+ * - n in G(pos) since last_kmer_start_position(n) < last_kmer_end_position(n) < first_kmer_start_position(n) <= pos
+ * - break n length-wise
+ *   - need to prove that the partially constructed nodes adjacent to n will reference the correct split
+ *    - yes: neighbours of split before n' are fully defined 
+ *    - yes: neighbours of n' are fully defined
+ *    - yes: neighbours of post n' are partially defined iff they only connect to post n' -> connect to post n' post-split
+ *  
+ * Leaf collapse:
+ *    * - - - - -   <- leaf     
+ *                \    
+ * * - * - * - * - * - ?
+ *                 ^
+ *                root
+ * 
+ * Conditions for backward leaf collapse match that of path collapse when the
+ * root node is considered to be the node adjacent to the leaf
+ * 
+ *     - - - - - *      <- collapse this leave into a main path
+ *   /
+ * * - * - * - * - * - ?
+ * 
+ * Similarly, forward leaf collapse requires fully defined path nodes up to last_kmer_end_position(leaf)
+ * 
+ * 
+ * @author Daniel Cameron
+ *
+ */
 public class PositionalDeBruijnGraphPathNodeGraphSimplifier implements Iterator<KmerPathNode> {
 	private final PeekingIterator<KmerPathNode> underlying;
 	private final int k;
+	private final int maxSupportWidth;
 	private final int maxCollapseLength;
 	private final int maxBaseMismatchForCollapse;
 	private final boolean collapseBubblesOnly;
-	private final Queue<KmerPathNode> outputBuffer = new ArrayDeque<KmerPathNode>();
-	private final Set<KmerPathNode> active = new HashSet<KmerPathNode>();
-	private final PriorityQueue<KmerPathNode> activeBranchStart = new PriorityQueue<KmerPathNode>(1024, KmerPathNode.ByEndPosition);
-	private final PriorityQueue<KmerPathNode> activeLeafStartAnchored = new PriorityQueue<KmerPathNode>(1024, KmerPathNode.ByEndPosition);
-	private final PriorityQueue<KmerPathNode> activeLeafEndAnchored = new PriorityQueue<KmerPathNode>(1024, KmerPathNode.ByFirstKmerStartPosition);
+	private final PriorityQueue<KmerPathNode> processed = new PriorityQueue<KmerPathNode>(1024, KmerPathNode.ByFirstKmerStartPosition);
+	private final PriorityQueue<UnprocessedNode> active = new PriorityQueue<UnprocessedNode>(1024, ByProcessingCompletePosition);
+	/**
+	 * Unprocessed forward branching leaves
+	 */
+	private final PriorityQueue<UnprocessedNode> activeLeafForward = new PriorityQueue<UnprocessedNode>(1024, ByForwardLeafProcessingPosition);
+	/**
+	 * Unprocessed end of possible branches.
+	 * Since nodes are added in 
+	 */
+	private final PriorityQueue<UnprocessedNode> activeBranchEnd = new PriorityQueue<UnprocessedNode>(1024, ByBranchEndProcessingPosition);
+	/**
+	 * Unprocessed backward branching leaves
+	 */
+	private final PriorityQueue<UnprocessedNode> activeLeafBackward = new PriorityQueue<UnprocessedNode>(1024, ByBackwardLeafProcessingPosition);
+	/**
+	 * Position at which all nodes starting at or this position have been loaded
+	 */
 	private int currentPosition = Integer.MIN_VALUE;
-	public PositionalDeBruijnGraphPathNodeGraphSimplifier(Iterator<KmerPathNode> it, int k, int maxCollapseLength, boolean collapseBubblesOnly, int maxBaseMismatchForCollapse) {
+	public PositionalDeBruijnGraphPathNodeGraphSimplifier(Iterator<KmerPathNode> it, int k, int maxSupportWidth, int maxCollapseLength, boolean collapseBubblesOnly, int maxBaseMismatchForCollapse) {
 		this.underlying = Iterators.peekingIterator(it);
 		this.k = k;
+		this.maxSupportWidth = maxSupportWidth;
 		this.maxCollapseLength = maxCollapseLength;
 		this.maxBaseMismatchForCollapse = maxBaseMismatchForCollapse;
 		this.collapseBubblesOnly = collapseBubblesOnly;
 	}
 	@Override
 	public boolean hasNext() {
+		if (!processed.isEmpty()) return true;
 		ensureBuffer();
-		return !outputBuffer.isEmpty();
+		return !processed.isEmpty();
 	}
 	@Override
 	public KmerPathNode next() {
 		ensureBuffer();
-		return outputBuffer.poll();
+		return processed.poll();
 	}
 	private void ensureBuffer() {
-		if (!outputBuffer.isEmpty()) return;
-		// load nodes into graph until we are guaranteed
-		// to be able to traverse all branches/leaves
-		// rooted at the first node
-		if (underlying.hasNext()) {
-			currentPosition = underlying.peek().startPosition(0);
-			addToGraph();
-		} else {
-			currentPosition = Integer.MAX_VALUE;
+		while (currentPosition < Integer.MAX_VALUE && processed.isEmpty() || processed.peek().endPosition() > currentPosition - maxCollapseLength - 1
+				/* maxSupportWidth required since backward leaves are delayed until the next node is emitted which could be as late as maxSupportWidth
+				 * after leaf node is emitted.
+				 */
+				- maxSupportWidth) {
+			// load nodes into graph until we are guaranteed
+			// to be able to traverse all branches/leaves
+			// rooted at the first node
+			if (underlying.hasNext()) {
+				currentPosition = underlying.peek().startPosition(0);
+				addToGraph();
+			} else {
+				currentPosition = Integer.MAX_VALUE;
+			}
+			collapse();
+			while (!active.isEmpty() && active.peek().processingCompletePosition <= currentPosition) {
+				processed.add(active.poll().node);
+			}
 		}
 	}
 	private void addToGraph() {
 		while (underlying.hasNext() && underlying.peek().startPosition(0) <= currentPosition) {
-			KmerPathNode pn = underlying.next();
-			active.add(pn);
-			if (pn.nextList.size() == 0 && pn.prevList.size() == 1) {
-				activeLeafStartAnchored.add(pn);
-			} else if (pn.prevList.size() == 0 && pn.nextList.size() == 1) {
-				activeLeafEndAnchored.add(pn);
-			} else if (pn.nextList.size() > 1) {
-				activeBranchStart.add(pn);
-			}
+			addToQueues(underlying.next());
 		}
 	}
-	private void process() {
-		// all paths starting at or before currentPosition are in our graph
-		collapseActiveBranches();
-		
-		//collapseActiveBranchesStartEndingBefore(startPosition - maxCollapseLength - 1);
-		//collapseActiveStartAnchoredLeavesEndingBefore(startPosition - maxCollapseLength - 1);
+	private void addToQueues(KmerPathNode node) {
+		UnprocessedNode un = new UnprocessedNode(node);
+		// can't use more restrictive conditions without
+		// fully decoding edge validity intervals
+		// with (eg EdgeIntervalIterator)
+		// additionally, if nodes are subsequently
+		// merge into this, edge counts in both
+		// directions can increase
+		if (node.prev().size() > 0) {
+			// forward leaf = 1 prev, 0 next
+			activeLeafForward.add(un);
+			// branch end candidate = 2+ prev
+			activeBranchEnd.add(un);
+		}
+		if (node.next().size() > 0) {
+			// backward leaf = 0 prev, 1 next
+			activeLeafBackward.add(un);
+		}
+		active.add(un);
 	}
-	private void collapseActiveBranches() {
-		//while (!activeBranchStart.isEmpty() && activeBranchStart.peek().endPosition() < 
+	private int collapse() {
+		int collapseCount = collapseBranches();
+		collapseCount += collapseForwardLeaves();
+		collapseCount += collapseBackwardLeaves();
+		return collapseCount;
 	}
+	private int collapseBranches() {
+		int collapseCount = 0;
+		while (!activeBranchEnd.isEmpty() && activeBranchEnd.peek().branchEndProcessingPosition <= currentPosition) {
+			UnprocessedNode node = activeBranchEnd.poll();
+			if (tryCollapseBranch(node.node)) {
+				collapseCount++;
+			}
+		}
+		return collapseCount;
+	}
+	private int collapseForwardLeaves() {
+		int collapseCount = 0;
+		while (!activeLeafForward.isEmpty() && activeLeafForward.peek().forwardLeafProcessingPosition <= currentPosition) {
+			UnprocessedNode node = activeLeafForward.poll();
+			if (tryCollapseForwardLeaf(node.node)) {
+				collapseCount++;
+			}
+		}
+		return collapseCount;
+	}
+	private int collapseBackwardLeaves() {
+		int collapseCount = 0;
+		while (!activeBranchEnd.isEmpty() && activeBranchEnd.peek().backwardLeafProcessingPosition <= currentPosition) {
+			UnprocessedNode node = activeLeafBackward.poll();
+			if (tryCollapseBackwardLeaf(node.node)) {
+				collapseCount++;
+			}
+		}
+		return collapseCount;
+	}
+	private boolean tryCollapseForwardLeaf(KmerPathNode node) {
+		if (!node.isValid()) return false;
+		// determine the intervals in which this node is a forward leaf
+		EdgeIntervalIterator eii = new EdgeIntervalIterator(node);
+		IntArrayList starts = new IntArrayList();
+		IntArrayList ends = new IntArrayList();
+		while (eii.isValid()) {
+			if (eii.currentNext().size() == 0 && eii.currentPrev().size() == 1) {
+				starts.add(eii.currentFirstKmerStartPosition());
+				ends.add(eii.currentFirstKmerEndPosition());
+			}
+			eii.advance();
+		}
+		//tryCollapseForwardLeaf(node, start, end);
+		throw new RuntimeException("NYI");
+	}
+	private boolean tryCollapseBackwardLeaf(KmerPathNode node) {
+		if (!node.isValid()) return false;
+		throw new RuntimeException("NYI");
+	}
+	private boolean tryCollapseBranch(KmerPathNode node) {
+		if (!node.isValid()) return false;
+		throw new RuntimeException("NYI");
+	}
+	/**
+	 * Processing positions are the earliest input position
+	 * at which the node can be processed.
+	 * 
+	 * These inital snapshot positions ensure that 
+	 * invariants are not void
+	 * 
+	 * Snapshot values are required to ensure that PriorityQueue invariants
+	 * 
+	 * @author Daniel Cameron
+	 *
+	 */
+	private class UnprocessedNode {
+		public int calculateBranchEndProcessingPosition() {
+			return node.endPosition();
+		}
+		public int calculateForwardLeafProcessingPosition() {
+			return node.endPosition(0);
+		}
+		public int calculateBackwardLeafProcessingPosition() {
+			return maxFirstKmerEndPositionOfSuccessor();
+		}
+		/**
+		 * Position at which no more processing will be performed
+		 * @return
+		 */
+		public int calcualteProcessingCompletePosition() {
+			return maxFirstKmerEndPositionOfSuccessor();
+		}
+		private int maxFirstKmerEndPositionOfSuccessor() {
+			int pos = Integer.MIN_VALUE;
+			for (KmerPathNode pn : node.next()) {
+				pos = Math.max(pos, pn.endPosition(0));
+			}
+			return pos;
+		}
+		public final KmerPathNode node;
+		public int branchEndProcessingPosition;
+		public int forwardLeafProcessingPosition;
+		public int backwardLeafProcessingPosition;
+		public int processingCompletePosition;
+		public UnprocessedNode(KmerPathNode node) {
+			this.node = node;
+			this.branchEndProcessingPosition = calculateBranchEndProcessingPosition();
+			this.forwardLeafProcessingPosition = calculateForwardLeafProcessingPosition();
+			this.backwardLeafProcessingPosition = calculateBackwardLeafProcessingPosition();
+			this.processingCompletePosition = calcualteProcessingCompletePosition();
+		}
+	}
+	private static Ordering<UnprocessedNode> ByBranchEndProcessingPosition = new Ordering<UnprocessedNode>() {
+		@Override
+		public int compare(UnprocessedNode left, UnprocessedNode right) {
+			return Ints.compare(left.branchEndProcessingPosition, right.branchEndProcessingPosition);
+		}
+	};
+	private static Ordering<UnprocessedNode> ByForwardLeafProcessingPosition = new Ordering<UnprocessedNode>() {
+		@Override
+		public int compare(UnprocessedNode left, UnprocessedNode right) {
+			return Ints.compare(left.forwardLeafProcessingPosition, right.forwardLeafProcessingPosition);
+		}
+	};
+	private static Ordering<UnprocessedNode> ByBackwardLeafProcessingPosition = new Ordering<UnprocessedNode>() {
+		@Override
+		public int compare(UnprocessedNode left, UnprocessedNode right) {
+			return Ints.compare(left.backwardLeafProcessingPosition, right.backwardLeafProcessingPosition);
+		}
+	};
+	private static Ordering<UnprocessedNode> ByProcessingCompletePosition = new Ordering<UnprocessedNode>() {
+		@Override
+		public int compare(UnprocessedNode left, UnprocessedNode right) {
+			return Ints.compare(left.processingCompletePosition, right.processingCompletePosition);
+		}
+	};
 }
