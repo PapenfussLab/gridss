913ac2aa1c852a63ae676148e99ac0a93ebb58f2
Daniel Cameron <daniel.l.cameron@gmail.com>
2015-11-16 16:41:01 +1100

    Added small event filter

--- scripts/multiregion-tumour-example.R
+++ scripts/multiregion-tumour-example.R
@@ -7,10 +7,10 @@
 source("libgridss.R") # found in src/test/r
 
 vcf <- readVcf("C:/dev/data/FR07935989_dups.gridss.filt.vcf", "hg19")
+matevcf <- vcf[as.character(info(vcf)$MATEID),]
 df <- gridss.vcftodf(vcf, allColumns=TRUE)
 # this example assume that the normal was assigned to category 0
 # and two regions exist
-
 library(ggplot2)
 ggplot(df) + aes(x=Q0 / df$QUAL) + geom_histogram(binwidth=0.01) + scale_x_continuous(limits=c(0,1))
 ggplot(df) + aes(x=Q1 / df$QUAL) + geom_histogram(binwidth=0.01) + scale_x_continuous(limits=c(0,1))
@@ -25,6 +25,9 @@ info(vcf)$SOMATIC <- isSomatic
 info(vcf)$SOMATIC1 <- isSomatic & df$Q1 / df$QUAL >= 0.10
 info(vcf)$SOMATIC2 <- isSomatic & df$Q2 / df$QUAL >= 0.10
 
-writeVcf(vcf[isSomatic], "FR07935989_dups.gridss.filt.somatic.vcf")
+# note: this just checks that breakends are close to each other, not the orientation
+# of the breakends and will filter large-scale events that just happen to have
+# nearby breakend
+isSmallEvent <- seqnames(vcf) == seqnames(matevcf) & start(vcf) - start(matevcf) <= 50
 
-
+writeVcf(vcf[isSomatic & isSmallEvent], "FR07935989_dups.gridss.filt.somatic.vcf")
