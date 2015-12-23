36e162ac6db0319edfdc2c0df407136df0536fb4
Daniel Cameron <daniel.l.cameron@gmail.com>
2015-12-23 22:13:00 +1100

    Fixing failing test cases

--- src/test/java/au/edu/wehi/idsv/SpannedIndelEvidenceTest.java
+++ src/test/java/au/edu/wehi/idsv/SpannedIndelEvidenceTest.java
@@ -4,11 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.google.common.collect.Iterators;
 
-public class SpannedIndelEvidenceTest extends TestHelper {
-	
+import htsjdk.samtools.SAMRecord;
+
+
+public class SpannedIndelEvidenceTest extends TestHelper {	
 	@Test
 	public void qual_should_use_library_indel_distribution() {
-		fail();
+		SAMRecord r = Read(2, 1, "5M5D5M");
+		r.setMappingQuality(40);
+		SoftClipEvidenceIterator it = new SoftClipEvidenceIterator(SES(), Iterators.singletonIterator(r));
+		SpannedIndelEvidence e = (SpannedIndelEvidence)it.next();
+		it.close();
+		assertEquals(5, e.getBreakendQual(), 0.01);
+		assertEquals(5, e.getBreakpointQual(), 0.01);
 	}
 }
