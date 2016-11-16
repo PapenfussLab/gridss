23c6ee0607749822e17b96827b938d3375fd84b0
Daniel Cameron <cameron.d@wehi.edu.au>
2016-11-17 10:27:34 +1100

    Adding missing Defaults.java

---
+++ src/main/java/gridss/Defaults.java
@@ -0,0 +1,7 @@
+package gridss;
+
+public class Defaults {
+	public static boolean IGNORE_FILE_TIMESTAMPS = true;
+	public static int ASYNC_BUFFERS = 2;
+	public static int ASYNC_BUFFER_SIZE = 300;
+}
