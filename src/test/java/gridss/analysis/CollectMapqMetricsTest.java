9b6bee823d1b46cfa60f64e2fd80071503147a7e
d-cameron <daniel.l.cameron@gmail.com>
2017-07-11 15:13:41 +1000

    Added test case for #85

--- src/test/java/gridss/analysis/CollectMapqMetricsTest.java
+++ src/test/java/gridss/analysis/CollectMapqMetricsTest.java
@@ -1,9 +1,14 @@
 package gridss.analysis;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -11,10 +16,22 @@ import org.junit.Test;
 import com.google.common.collect.ImmutableList;
 
 import au.edu.wehi.idsv.IntermediateFilesTest;
+import au.edu.wehi.idsv.metrics.IdsvSamFileMetrics;
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.metrics.MetricsFile;
+import htsjdk.samtools.util.Histogram;
+import htsjdk.samtools.util.Histogram.Bin;
 import picard.analysis.SinglePassSamProgram;
 
 
 public class CollectMapqMetricsTest extends IntermediateFilesTest {
+	public static Histogram<Integer> loadHistogram(File file) throws FileNotFoundException, IOException {
+		try (FileReader reader  = new FileReader(file)) {
+			MetricsFile<MapqMetrics,Integer> metricsFile = new MetricsFile<MapqMetrics, Integer>();
+			metricsFile.read(reader);
+			return metricsFile.getHistogram();
+		}
+	}
 	@Test
 	public void should_allow_no_histogram() throws IOException {
 		CollectMapqMetrics cmm = new CollectMapqMetrics();
@@ -42,5 +59,28 @@ public class CollectMapqMetricsTest extends IntermediateFilesTest {
 		cmm.Histogram_FILE = histogram;
 		SinglePassSamProgram.makeItSo(cmm.INPUT, null, true, 0, ImmutableList.of(cmm));
 		assertTrue(histogram.isFile());
+	}
+	@Test
+	public void metrics_should_correspond_to_read_counts() throws IOException {
+		List<SAMRecord> reads = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			for (int j = 0; j <= i; j++) {
+				reads.add(withMapq(j, Read(0, 1 + j,  10))[0]);
+			}
+		}
+		createInput(reads);
+		CollectMapqMetrics cmm = new CollectMapqMetrics();
+		cmm.INPUT = input;
+		cmm.OUTPUT = new File(testFolder.getRoot(), "mapqmetrics.txt");
+		cmm.Histogram_FILE = new File(testFolder.getRoot(), "mapqhistogram.pdf");
+		SinglePassSamProgram.makeItSo(cmm.INPUT, null, true, 0, ImmutableList.of(cmm));
+		MapqMetrics metrics = IdsvSamFileMetrics.getMapqMetrics(cmm.OUTPUT);
+		assertEquals(0, metrics.MIN_MAPQ);
+		assertEquals(49, metrics.MAX_MAPQ);
+		Histogram<Integer> histo = loadHistogram(cmm.OUTPUT);
+		assertEquals(50, histo.values().size());
+		for (Bin<Integer> bin : histo.values()) {
+			assertEquals((int)bin.getId(), (int)bin.getValue());
+		}
 	}
 }
