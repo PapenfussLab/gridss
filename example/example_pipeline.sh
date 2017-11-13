e75f887b75a3dce5b4af3a168359fab106c6a3e7
Daniel Cameron <cameron.d@wehi.edu.au>
2017-11-13 17:16:37 +1100

    Added example of an efficient NGS pipeline that runs GRIDSS Changed min soft-clip length to match ExtractSVReads default

--- example/example_pipeline.sh
+++ example/example_pipeline.sh
@@ -8,32 +8,46 @@
 # - some steps can be skipped since we know what aligner and settings were used
 #
 
+# environment modules
+module remove samtools bwa java R
+module add bwa/0.7.15 samtools/1.6 java/1.8.0_131 R/3.4.2 mbuffer
+#cd ~/test/
+
 FQ1=example.R1.fq
 FQ2=example.R2.fq
 REFERENCE=~/reference_genomes/human/hg19.fa
-INPUT=example.input.bam
-OUTPUT=example.sv.vcf
+INPUT=pipelined.example.input.bam
+OUTPUT=pipelined.example.sv.vcf
 RAW_GRIDSS_ASSEMBLY=${OUTPUT/.sv.vcf/.gridss.assembly.bam}
 GRIDSS_JAR=~/bin/gridss-1.5.0-SNAPSHOT-jar-with-dependencies.jar
-GRIDSS_JVM_ARGS="-ea
+GRIDSS_JVM_ARGS="
 	-Dsamjdk.use_async_io_read_samtools=true 
 	-Dsamjdk.use_async_io_write_samtools=true 
 	-Dsamjdk.use_async_io_write_tribble=true"
+# Enabling assertions performs GRIDSS data consistency checks.
+# Removing these can slightly improve performance
+GRIDSS_JVM_ARGS="$GRIDSS_JVM_ARGS -ea" 
 GRIDSS_THRESHOLD_COVERAGE=10000
 LOG_PREFIX=log.gridss.pipeline.$HOSTNAME.$$
+
+echo Starting pipelined GRIDSS example
+# Optional performance logging
+dstat -cdmnpry -t --vm --top-cpu --noheader --output $LOG_PREFIX.timing.log &
+TIMING_PID=$!
+
 ####################
 # Environment/external tools sanity checks
-
-if [[ ! -f "$PICARD" ]] ; then
-	echo "The PICARD environment variable does not point to the picard tools jar. Please add \$PICARD and point it to your picard tools location"
+if [[ ! -f "$REFERENCE" ]] ; then
+	echo "Missing reference genome $REFERENCE"
+	exit 1
+fi
+if [[ ! "$(samtools --version | head -1 2>/dev/null )" =~ "1.6" ]] ; then
+	# samtools markdup is in samtools version 1.6
+	echo "Missing samtools v1.6. Please add to PATH"
 	exit 1
 fi
 if ! which bwa >/dev/null 2>&1 ; then
 	echo "Missing bwa. Please add to PATH"
-	exit 1
-fi
-if [[ ! -f "$REFERENCE" ]] ; then
-	echo "Missing reference genome $REFERENCE"
 	exit 1
 fi
 if [[ ! -f "$REFERENCE.bwt" ]] ; then
@@ -63,20 +77,21 @@ mkdir -p $INPUT_WORKING_DIR
 if [[ ! -f $INPUT ]] ; then
 	if [[ ! -f $INPUT_WORKING_PREFIX.unsorted.bam ]] ; then
 		# First step of pipeline: alignment
-		bwa mem -t $(nproc) -Y $REFERENCE $FQ1 $FQ2 2> $LOG_PREFIX.1.1.bwa.log | \
-		samtools fixmate -m - - 2> $LOG_PREFIX.1.2.fixmate.log | \
-		# Skip compression since we're in a pipe
-		# RECALCULATE_SA_SUPPLEMENTARY not needed since we aligned with -Y and without -M
-		# SOFTEN_HARD_CLIPS not needed since we supplied the -Y flag to bwa
-		# FIX_MATE_INFORMATION not needed since we used bwa
-		java $GRIDSS_JVM_ARGS -Dsamjdk.create_index=false \
+		nice bwa mem -t $(nproc) -Y $REFERENCE $FQ1 $FQ2 2> $LOG_PREFIX.1.bwa.log | \
+		# buffer bwa output to improve pipe performance
+		mbuffer -m 256M | \
+		# COMPRESSION_LEVEL: skip compression since we're in a pipe
+		# RECALCULATE_SA_SUPPLEMENTARY: not needed since we aligned with -Y and without -M
+		# SOFTEN_HARD_CLIPS:  not needed since we supplied the -Y flag to bwa
+		# FIX_MATE_INFORMATION: not needed since we used bwa
+		java -Xmx1G $GRIDSS_JVM_ARGS -Dsamjdk.create_index=false \
 			-cp $GRIDSS_JAR gridss.ComputeSamTags \
+			COMPRESSION_LEVEL=0 \
 			REFERENCE_SEQUENCE=$REFERENCE \
 			WORKING_DIR=$INPUT_WORKING_DIR \
 			TMP_DIR=$INPUT_WORKING_DIR \
 			I=/dev/stdin \
 			O=/dev/stdout \
-			COMPRESSION_LEVEL=0 \
 			RECALCULATE_SA_SUPPLEMENTARY=false \
 			SOFTEN_HARD_CLIPS=false \
 			FIX_MATE_INFORMATION=false \
@@ -86,30 +101,65 @@ if [[ ! -f $INPUT ]] ; then
 			TAGS=MC \
 			TAGS=MQ \
 			AS=true \
-			2> $LOG_PREFIX.1.3.ComputeSamTags.log | \
+			2> $LOG_PREFIX.1.ComputeSamTags.log | \
 		# Although bwa reports split reads, you can identify additional split reads
 		# by feeding the soft clipped reads back to bwa
-		java $GRIDSS_JVM_ARGS -Dsamjdk.create_index=false \
+		# Note that ALIGNER_STREAMING=true changes the order of the
+		# records but that's ok here as we're going to sort it in the next
+		# step
+		java -Xmx2G $GRIDSS_JVM_ARGS -Dsamjdk.create_index=false \
 			-cp $GRIDSS_JAR gridss.SoftClipsToSplitReads \
+			COMPRESSION_LEVEL=0 \
 			REFERENCE_SEQUENCE=$REFERENCE \
 			I=/dev/stdin \
 			O=/dev/stdout \
 			ALIGNER_STREAMING=true \
 			WORKER_THREADS=$(nproc) \
-			2> $LOG_PREFIX.1.4.SoftClipsToSplitReads.log > $INPUT_WORKING_PREFIX.unsorted.bam
+			2> $LOG_PREFIX.1.SoftClipsToSplitReads.log | \
+		# Using non-POSIX process substitution feature of bash to fork the output stream
+		# Unfortunately, the current version of HTSJDK has relatively slow compression.
+		# We work around this by deferring compression to samtools in a different process.
+		# As the unsorted BAM is only a temporary file, we're using fast compresison
+		# bwa processes records in batches. Without buffering the output
+		# bwa will stall waiting for the downstream pipe to process the output
+		# before aligning the next batch
+		# NB: two additional threads have been added to prevent this step bottlenecking the pipe
+		tee >(samtools fixmate -@ 4 -m - $INPUT_WORKING_PREFIX.unsorted.bam 2> $LOG_PREFIX.1.fixmate.log) | \
+		# THRESHOLD_COVERAGE=gridss.properties/maxCoverage
+		# REFERENCE_SEQUENCE: don't supply since our metrics don't care about order and Picard crashes
+		#  if we supply out of order records with a reference sequence
+		java -Xmx1G $GRIDSS_JVM_ARGS -cp $GRIDSS_JAR \
+			gridss.analysis.CollectGridssMetrics \
+			ASSUME_SORTED=true \
+			I=/dev/stdin \
+			O=$INPUT_WORKING_PREFIX \
+			THRESHOLD_COVERAGE=$GRIDSS_THRESHOLD_COVERAGE \
+			2> $LOG_PREFIX.1.CollectGridssMetrics.log
 	fi
 	# Second step of pipeline: sorting
 	samtools sort -@ $(nproc) -O BAM -l 0 -T $INPUT_WORKING_DIR $INPUT_WORKING_PREFIX.unsorted.bam 2> $LOG_PREFIX.2.1.sort.log | \
 	samtools markdup - - -O BAM 2> $LOG_PREFIX.2.2.markdup.log | \
-	# THRESHOLD_COVERAGE=gridss.properties/maxCoverage
-	# Using non-POSIX-compatble process substitution feature of bash to fork the output stream
-	tee >(java $GRIDSS_JVM_ARGS -cp $GRIDSS_JAR gridss.analysis.CollectGridssMetrics REFERENCE_SEQUENCE=$REFERENCE I=/dev/stdin O=$INPUT_WORKING_PREFIX THRESHOLD_COVERAGE=$GRIDSS_THRESHOLD_COVERAGE 2> $LOG_PREFIX.2.4.CollectGridssMetrics.log ) > $INPUT && \
+	# Using non-POSIX process substitution feature of bash to fork the output stream
+	# Strip out R2, Q2 tags to save space. These can be rehydrated from
+	# the mate information and GRIDSS only needs them for the extracted reads
+	# MC/MQ could also be stripped but they do not significantly change the file size
+	tee >(samtools view -b -x R2 -x Q2 - $INPUT 2> $LOG_PREFIX.2.4.striptags.log) | \
+	# MIN_CLIP_LENGTH=gridss.properties/softclip.minLength
+	# This step can be skipped if the R2/Q2 tags are retained. For most data sets,
+	# keeping the extracted SV reads with R2/Q2 and stripping the tags from the
+	# full BAM uses less space than retain the R2/Q2 tags.
+	java $GRIDSS_JVM_ARGS -Dsamjdk.create_index=true \
+		-cp $GRIDSS_JAR gridss.ExtractSVReads \
+		REFERENCE_SEQUENCE=$REFERENCE \
+		I=/dev/stdin \
+		O=$INPUT_WORKING_PREFIX.sv.bam \
+		METRICS_OUTPUT=$INPUT_WORKING_PREFIX.sv_metrics \
+		INSERT_SIZE_METRICS=$INPUT_WORKING_PREFIX.insert_size_metrics \
+		UNMAPPED_READS=false MIN_CLIP_LENGTH=5 2> $LOG_PREFIX.2.3.ExtractSVReads.log && \
 	samtools index $INPUT 2> $LOG_PREFIX.2.5.index.log && \
 	# the unsorted input file can now be deleted
-	rm $INPUT_WORKING_PREFIX.unsorted.bam
-	###
-	# Optional step: extraction of SV-supporting reads
-	# This speeds up GRIDSS and is also useful for manual inspection of putative SVs
+	rm $INPUT_WORKING_PREFIX.unsorted.bam && \
+	samtools index $INPUT_WORKING_PREFIX.sv.bam 2> $LOG_PREFIX.2.6.index.log
 fi
 ####################
 # Insert other steps here
@@ -187,14 +237,8 @@ if [[ ! -f $OUTPUT ]] ; then
 	2> $LOG_PREFIX.5.1.AnnotateVariants.log 
 fi
 
-
-
-
-
-
-
-
-
+# Optional performance logging
+kill $TIMING_PID
 
 
 
