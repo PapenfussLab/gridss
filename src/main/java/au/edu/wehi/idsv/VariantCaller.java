f1babf35a828e19f086f5bf058ebe8d56787afb1
d-cameron <daniel.l.cameron@gmail.com>
2017-12-07 00:05:26 +1100

    gridss.IdentifyVariants implementation of #25 Currently missing required test coverage

--- src/main/java/au/edu/wehi/idsv/VariantCaller.java
+++ src/main/java/au/edu/wehi/idsv/VariantCaller.java
@@ -97,9 +97,9 @@ public class VariantCaller {
 		try (VariantCallIterator rawit = new VariantCallIterator(es, chunk, chunkNumber)) {
 			try (VariantContextWriter vcfWriter = processContext.getVariantContextWriter(tmp, false)) {
 				log.info("Start ", msg);
-				try (AsyncBufferedIterator<VariantContextDirectedBreakpoint> it = new AsyncBufferedIterator<>(rawit, "VariantCaller " + chunkMsg)) {
+				try (AsyncBufferedIterator<VariantContextDirectedEvidence> it = new AsyncBufferedIterator<>(rawit, "VariantCaller " + chunkMsg)) {
 					while (it.hasNext()) {
-						VariantContextDirectedBreakpoint loc = it.next();
+						VariantContextDirectedEvidence loc = it.next();
 						if (loc.getBreakendQual() >= processContext.getVariantCallingParameters().minScore || processContext.getVariantCallingParameters().writeFiltered) {
 							// If we're under min score with all possible evidence allocated, we're definitely going to fail
 							// when we restrict evidence to single breakpoint support
