41f06c5666a4d96ffd54d061c2794242deae70df
Daniel Cameron <cameron.d@wehi.edu.au>
2018-02-12 16:50:19 +1100

    Fixed async buffer size override to use -Dgridss.async.buffersize

--- src/main/java/gridss/Defaults.java
+++ src/main/java/gridss/Defaults.java
@@ -21,7 +21,7 @@ public class Defaults {
 	static {
 		IGNORE_FILE_TIMESTAMPS = Boolean.valueOf(System.getProperty("gridss.ignoreTimestamps", "true"));
 		ASYNC_BUFFERS = Integer.parseInt(System.getProperty("gridss.async.buffers", "2"));
-		ASYNC_BUFFER_SIZE = Integer.parseInt(System.getProperty("gridss.async.buffers", "300"));
+		ASYNC_BUFFER_SIZE = Integer.parseInt(System.getProperty("gridss.async.buffersize", "300"));
 		DELETE_TEMPORARY_FILES = !Boolean.valueOf(System.getProperty("gridss.keepTempFiles", "false"));
 		SUPPRESS_DATA_ERROR_MESSAGES_AFTER = Integer.parseInt(System.getProperty("gridss.logSpamLimit", "100"));
 		WRITE_ZERO_OR_EMTPY_VCF_FIELDS = Boolean.valueOf(System.getProperty("gridss.writeZeroOrEmptyVcfFields", "true"));
