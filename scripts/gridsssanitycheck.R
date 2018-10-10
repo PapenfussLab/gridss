7d673d2ab326325138c8092cfce76d22529e014b
Daniel Cameron <daniel.l.cameron@gmail.com>
2018-10-10 23:17:45 +1100

    Updated sanity checking script

--- scripts/gridsssanitycheck.R
+++ scripts/gridsssanitycheck.R
@@ -13,12 +13,42 @@ if (!file.exists(args[2])) {
 	q(save="no", status=1)
 }
 library(VariantAnnotation)
-#library(StructuralVariantAnnotation)
 
 
 errorCount <- 0
 margin <- 0.04 # margin of error allowed due to VCF rounding to 2dp
 vcf <- readVcf(args[2], "unknown")
+
+vcf = vcf[!is.na(info(vcf)$PARID)] # TODO expand script to include single breakends
+
+######
+# Annotation breakdown validations
+
+# QUAL INFO breakdown
+mismatchedField = abs(rowRanges(vcf)$QUAL - (info(vcf)$ASQ + info(vcf)$RASQ + info(vcf)$CASQ + info(vcf)$SRQ + info(vcf)$IQ + info(vcf)$RPQ)) > margin
+if (any(mismatchedField)) {
+	errorCount <- errorCount + 1
+	write(paste0("INFO quality score breakdown does not match total for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
+}
+# QUAL FORMAT breakdown
+mismatchedField = rowSums(abs(geno(vcf)$ASQ + geno(vcf)$RASQ + geno(vcf)$CASQ + geno(vcf)$SRQ + geno(vcf)$IQ + geno(vcf)$RPQ - geno(vcf)$QUAL)) > margin
+if (any(mismatchedField)) {
+	errorCount <- errorCount + 1
+	write(paste0("FORMAT quality score breakdown does not match FORMAT QUAL total for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
+}
+# BQ INFO breakdown
+mismatchedField = abs(info(vcf)$BQ - (info(vcf)$BAQ + info(vcf)$BSCQ + info(vcf)$BUMQ)) > margin
+if (any(mismatchedField)) {
+	errorCount <- errorCount + 1
+	write(paste0("INFO BQ breakend quality score breakdown does not match total for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
+}
+# BQ FORMAT breakdown
+mismatchedField = rowSums(abs(geno(vcf)$BAQ + geno(vcf)$BSCQ + geno(vcf)$BUMQ - geno(vcf)$BQ)) > margin
+if (any(mismatchedField)) {
+	errorCount <- errorCount + 1
+	write(paste0("FORMAT quality score breakdown does not match FORMAT QUAL total for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
+}
+
 
 # Breakends are partnered
 missingPARID <- row.names(vcf)[!(info(vcf)$PARID %in% row.names(vcf))]
@@ -44,11 +74,13 @@ if (any(mismatchedField)) {
 breakpointFieldName <- c("CAS", "AS", "RAS", "SR", "RP", "IC", "ASRP", "ASSR", "ASQ", "RASQ", "SRQ", "RPQ", "IQ", "CQ")
 breakpointFieldPartner <- c("CAS", "RAS", "AS", "SR", "RP", "IC", "ASRP", "ASSR", "RASQ", "ASQ", "SRQ", "RPQ", "IQ", "CQ")
 # INFO matches partner
+mismatchedFieldList = data.frame(field=character(0), count=numeric(0))
 for (i in seq_along(breakpointFieldName)) {
 	mismatchedField = abs(info(vcf)[[breakpointFieldName[i]]] - info(pvcf)[[breakpointFieldPartner[i]]]) > margin
 	if (any(mismatchedField)) {
 		errorCount <- errorCount + 1
 		write(paste0(breakpointFieldName[i], ": INFO does not match partner for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
+		mismatchedFieldList = rbind(mismatchedFieldList, data.frame(field=breakpointFieldName[i], sum(mismatchedField)))
 	}
 }
 # FORMAT matches partner
@@ -70,30 +102,6 @@ for (fieldName in c("SR", "RP", "ASRP", "ASSR", "RASQ", "ASQ", "SRQ", "RPQ", "RE
 		errorCount <- errorCount + 1
 		write(paste0(fieldName, ": INFO does not match FORMAT for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
 	}
-}
-# QUAL INFO breakdown
-mismatchedField = abs(rowRanges(vcf)$QUAL - (info(vcf)$ASQ + info(vcf)$RASQ + info(vcf)$CASQ + info(vcf)$SRQ + info(vcf)$IQ + info(vcf)$RPQ)) > margin
-if (any(mismatchedField)) {
-	errorCount <- errorCount + 1
-	write(paste0("INFO quality score breakdown does not match total for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
-}
-# QUAL FORMAT breakdown
-mismatchedField = rowSums(abs(geno(vcf)$ASQ + geno(vcf)$RASQ + geno(vcf)$CASQ + geno(vcf)$SRQ + geno(vcf)$IQ + geno(vcf)$RPQ - geno(vcf)$QUAL)) > margin
-if (any(mismatchedField)) {
-	errorCount <- errorCount + 1
-	write(paste0("FORMAT quality score breakdown does not match FORMAT QUAL total for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
-}
-# BQ INFO breakdown
-mismatchedField = abs(rowRanges(vcf)$BQ - (info(vcf)$BAQ + info(vcf)$BSCQ + info(vcf)$BUMQ)) > margin
-if (any(mismatchedField)) {
-	errorCount <- errorCount + 1
-	write(paste0("INFO BQ breakend quality score breakdown does not match total for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
-}
-# BQ FORMAT breakdown
-mismatchedField = rowSums(abs(geno(vcf)$BAQ + geno(vcf)$BSCQ + geno(vcf)$BUMQ - geno(vcf)$BQ)) > margin
-if (any(mismatchedField)) {
-	errorCount <- errorCount + 1
-	write(paste0("FORMAT quality score breakdown does not match FORMAT QUAL total for: ", paste(row.names(vcf)[mismatchedField], collapse=", ")), stdout())
 }
 # CIPOS matches CIRPOS
 mismatchedField = all(info(vcf)$CIPOS != info(pvcf)$CIRPOS)
