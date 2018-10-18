1c5130774bba1fafedeb5c0639a56d227b67fde6
Daniel Cameron <daniel.l.cameron@gmail.com>
2018-10-18 15:22:57 +1100

    Updated cohort script GRIDSS version to latest

--- scripts/cohort_analysis/4_cleanup.py
+++ scripts/cohort_analysis/4_cleanup.py
@@ -4,7 +4,7 @@
 
 rm scripts/*
 rm output/*
-rm gridss-1.7.1-gridss-jar-with-dependencies.jar
+rm gridss-2.0.1-gridss-jar-with-dependencies.jar
 rm data/ENCODE_blacklist_hg19/ENCFF001TDO.bed
 
 # rm data/hg19/*
