d361803eb19563d205b2b81df3e1f49bc23d5dfb
Daniel Cameron <d-cameron@github.com>
2019-01-24 14:25:19 +1100

    Performance improvement to PON generation code

--- src/test/java/au/edu/wehi/BedpeMergingCounterTest.java
+++ src/test/java/au/edu/wehi/BedpeMergingCounterTest.java
@@ -17,8 +17,8 @@ public class BedpeMergingCounterTest extends TestHelper {
     public void shouldIgnoreNominal() throws IOException {
         List<Pair<BreakpointSummary, Integer>> result = new ArrayList<>();
         BedpeMergingCounter c = new BedpeMergingCounter();
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 2, 1, 3, 0, BWD, 3, 3, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 0, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 2, 1, 3, 1, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 1, BWD, 3, 3, 3)));
         result.addAll(c.finish());
         assertEquals(1, result.size());
         assertEquals(2, (int)result.get(0).getSecond());
@@ -27,8 +27,8 @@ public class BedpeMergingCounterTest extends TestHelper {
     public void shouldAddContained() throws IOException {
         List<Pair<BreakpointSummary, Integer>> result = new ArrayList<>();
         BedpeMergingCounter c = new BedpeMergingCounter();
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 1, 0, BWD, 3, 3, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 0, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 1, 1, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 1, BWD, 3, 3, 3)));
         result.addAll(c.finish());
         assertEquals(1, result.size());
         assertEquals(2, (int)result.get(0).getSecond());
@@ -37,8 +37,8 @@ public class BedpeMergingCounterTest extends TestHelper {
     public void shouldMergeContained() throws IOException {
         List<Pair<BreakpointSummary, Integer>> result = new ArrayList<>();
         BedpeMergingCounter c = new BedpeMergingCounter();
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 0, BWD, 3, 3, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 3, 3, 3, 0, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 1, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 3, 3, 3, 1, BWD, 3, 3, 3)));
         result.addAll(c.finish());
         assertEquals(1, result.size());
         assertEquals(2, (int)result.get(0).getSecond());
@@ -47,9 +47,9 @@ public class BedpeMergingCounterTest extends TestHelper {
     public void shouldChainMerge() throws IOException {
         List<Pair<BreakpointSummary, Integer>> result = new ArrayList<>();
         BedpeMergingCounter c = new BedpeMergingCounter();
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 1, 0, BWD, 3, 3, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 2,2,2, 0, BWD, 3, 3, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 3,3,4, 0, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 1, 1, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 2,2,2, 1, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 3,3,4, 1, BWD, 3, 3, 3)));
         result.addAll(c.finish());
         assertEquals(1, result.size());
         assertEquals(3, (int)result.get(0).getSecond());
@@ -58,9 +58,9 @@ public class BedpeMergingCounterTest extends TestHelper {
     public void shouldMergeRemote() throws IOException {
         List<Pair<BreakpointSummary, Integer>> result = new ArrayList<>();
         BedpeMergingCounter c = new BedpeMergingCounter();
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 0, BWD, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 0, BWD, 4)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 0, BWD, 5)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, BWD, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, BWD, 4)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, BWD, 5)));
         result.addAll(c.finish());
         assertEquals(1, result.size());
         assertEquals(3, (int)result.get(0).getSecond());
@@ -69,12 +69,21 @@ public class BedpeMergingCounterTest extends TestHelper {
     public void shouldNotMergeDifferent() throws IOException {
         List<Pair<BreakpointSummary, Integer>> result = new ArrayList<>();
         BedpeMergingCounter c = new BedpeMergingCounter();
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 0, BWD, 3, 3, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 0, FWD, 3, 3, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 5,5,5, 0, BWD, 3, 3, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, BWD, 1, 1, 3, 0, BWD, 3, 3, 3)));
-        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 0, BWD, 5,5,5)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 1, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 1, FWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 5,5,5, 1, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, BWD, 1, 1, 3, 1, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 1, 1, 3, 1, BWD, 5,5,5)));
         result.addAll(c.finish());
         assertEquals(5, result.size());
+    }
+    @Test
+    public void shouldIgnoreHighBreakends() throws IOException {
+        List<Pair<BreakpointSummary, Integer>> result = new ArrayList<>();
+        BedpeMergingCounter c = new BedpeMergingCounter();
+        result.addAll(c.process(new BreakpointSummary(1, FWD, 1, 1, 3, 0, BWD, 3, 3, 3)));
+        result.addAll(c.process(new BreakpointSummary(0, FWD, 10, 0, BWD, 2)));
+        result.addAll(c.finish());
+        assertEquals(0, result.size());
     }
 }
