2a75263e0d5518c6486d9a0557a6bcba4a613c1a
Daniel Cameron <daniel.l.cameron@gmail.com>
2019-07-15 12:34:20 +1000

    Partial refactor of command line script

--- example/gridss.sh
+++ example/gridss.sh
@@ -11,10 +11,25 @@ if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
     echo '`getopt --test` failed in this environment.'
     exit 1
 fi
-USAGE_MESSAGE="Usage: gridss.sh --reference <reference.fa> --output <output.vcf> --assembly <assembly.bam> [--threads n] [--jar gridss.jar] [--workingdir <directory>] [--jvmheap <threads * 4>g] [--blacklist <blacklist.bed>] input1.bam [input2.bam [...]]"
+USAGE_MESSAGE="
+Usage: gridss.sh --reference <reference.fa> --output <output.vcf> --assembly <assembly.bam> [--threads n] [--jar gridss.jar] [--workingdir <directory>] [--jvmheap 28g] [--blacklist <blacklist.bed>] [--steps All|PreProcess|Assemble|Call] [--configuration gridss.properties] [--maxcoverage 50000] input1.bam [input2.bam [...]]
 
-OPTIONS=r:o:a:t:j:w:b:
-LONGOPTS=reference:,output:,assembly:,threads:,jar:,workingdir:,jvmheap:,blacklist:
+	-r/--reference: reference genome to use. Must have a .fai index file and a bwa index.
+	-o/--output: output VCF.
+	-a/--assembly: location of the GRIDSS assembly BAM. This file will be created by GRIDSS.
+	-t/--threads: number of threads to use. Defaults to the number of cores available.
+	-j/--jar: location of GRIDSS jar
+	-w/--workingdir: directory to place GRIDSS intermediate and temporary files. .gridss.working subdirectories will be created. Defaults to the current directory.
+	-b/--blacklist: BED file containing regions to ignore
+	-s/--steps: processing steps to run. Defaults to all steps.
+	-c/--configuration: configuration file use to override default GRIDSS settings.
+	--jvmheap: size of JVM heap for assembly and variant calling. Defaults to 27.5g to ensure GRIDSS runs on all cloud instances with approximate 32gb memory including DNANexus azure:mem2_ssd1_x8.
+	--maxcoverage: maximum coverage. Regions with coverage in excess of this are ignored.
+	"
+	
+
+OPTIONS=r:o:a:t:j:w:b:s:c:
+LONGOPTS=reference:,output:,assembly:,threads:,jar:,workingdir:,jvmheap:,blacklist:,steps:,configuration:,maxcoverage:
 ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
     # e.g. return value is 1
@@ -29,12 +44,20 @@ output_vcf=""
 assembly=""
 threads=$(nproc)
 gridss_jar=""
-jvmheap=""
+jvmheap="28g"
 blacklist=""
+metricsrecords=10000000
+steps="All"
+config_file=""
+maxcoverage=50000
 while true; do
     case "$1" in
         -r|--reference)
             reference="$2"
+            shift 2
+            ;;
+		-s|--steps)
+            steps="${2,,}"
             shift 2
             ;;
 		-w|--workingdir)
@@ -66,6 +89,15 @@ while true; do
 			printf -v threads '%d' "$2" 2>/dev/null
 			shift 2
 			;;
+		-c|--configuration)
+			config_file=$2
+			shift 2
+			;;
+		--maxcoverage)
+			maxcoverage=$2
+			shift 2
+			;;
+			
 		--)
             shift
             break
@@ -76,53 +108,50 @@ while true; do
             ;;
     esac
 done
+do_preprocess=false
+do_assemble=false
+do_call=false
+if [[ "$steps" == *"all"* ]] ; then
+	do_preprocess=true
+	do_assemble=true
+	do_call=true
+fi
+if [[ "$steps" == *"preprocess"* ]] ; then
+	do_preprocess=true
+fi
+if [[ "$steps" == *"assemble"* ]] ; then
+	do_assemble=true
+fi
+if [[ "$steps" == *"call"* ]] ; then
+	do_call=true
+fi
+##### --jar
 if [[ ! -f $gridss_jar ]] ; then
 	echo "$USAGE_MESSAGE"  1>&2
 	echo "Unable to find GRIDSS jar. Specify location using the --jar command line argument" 1>&2
 	exit 2
 fi
-gridss_jar=$(readlink -f $gridss_jar || echo -n)
 echo "Using GRIDSS jar $gridss_jar" 1>&2
+##### --workingdir
 if [[ "$workingdir" == "" ]] ; then
 	echo "$USAGE_MESSAGE"  1>&2
 	echo "Working directory must be specified. Specify using the --workingdir command line argument" 1>&2
 	exit 3
-else
-	if [[ ! -d $workingdir ]] ; then
-		if ! mkdir -p $workingdir ; then
-			echo Unable to create working directory $workingdir 1>&2
-			exit 2
-		fi
+fi
+if [[ ! -d $workingdir ]] ; then
+	if ! mkdir -p $workingdir ; then
+		echo Unable to create working directory $workingdir 1>&2
+		exit 2
 	fi
-	workingdir=$(readlink -f $workingdir || echo -n)
-	echo "Using working directory $workingdir" 1>&2
 fi
-if [[ "$assembly" == "" ]] ; then
-	echo "$USAGE_MESSAGE"  1>&2
-	echo "Specify assembly bam location using the --assembly command line argument. Assembly location must be in a writeable directory." 1>&2
-	exit 4
-else
-	mkdir -p $(dirname $assembly) || echo "Unable to create directory $(dirname $assembly)" 1>&2
-fi
-assembly=$(readlink -f $assembly || echo -n)
-if [[ "$assembly" == "" ]] ; then
-	echo "$USAGE_MESSAGE"  1>&2
-	echo "Specify assembly bam location using the --assembly command line argument. Assembly location must be in a writeable directory." 1>&2
-	exit 4
-fi
-echo "Using assembly bam $assembly" 1>&2
-if [[ "$reference" == "" ]] ; then
-	echo "$USAGE_MESSAGE"  1>&2
-	echo "Specify reference location using the --reference command line argument" 1>&2
-	exit 5
-fi
-reference=$(readlink -f $reference || echo -n)
+echo "Using working directory $workingdir" 1>&2
+##### --reference
 if [[ ! -f "$reference" ]] ; then
 	echo "$USAGE_MESSAGE"  1>&2
-	echo "Missing reference genome $reference" 1>&2
+	echo "Missing reference genome $reference. Specify reference location using the --reference command line argument" 1>&2
 	exit 6
 fi
-if [[ ! -f ${reference}.fai ]] && [[ ! -f ${reference/.fa/.fai} ]] && [[ ! -f ${reference/.faasta/.fai} ]]  ; then
+if [[ ! -f ${reference}.fai ]] && [[ ! -f $(basename $reference .fa).fai ]] && [[ ! -f $(basename $reference .fasta).fai ]]  ; then
 	echo "$USAGE_MESSAGE"  1>&2
 	echo "Unable to find fai index for reference genome." 1>&2
 	echo "Please create using `samtools faidx $reference`" 1>&2
@@ -135,18 +164,29 @@ if [[ ! -f ${reference}.bwt  ]] ; then
 	exit 8
 fi
 echo "Using reference genome $reference" 1>&2
-if [[ "$output_vcf" == "" ]] ; then
-	echo "$USAGE_MESSAGE"  1>&2
-	echo "Output VCF not specified. Use --output to specify output file." 1>&2
-	exit 9
+
+##### --assembly
+if [[ $do_assemble == "true" ]] ; then
+	if [[ "$assembly" == "" ]] ; then
+		echo "$USAGE_MESSAGE"  1>&2
+		echo "Specify assembly bam location using the --assembly command line argument. Assembly location must be in a writeable directory." 1>&2
+		exit 4
+	fi
+	mkdir -p $(dirname $assembly) || echo "Unable to create directory $(dirname $assembly) for assembly BAM." 1>&2
+	echo "Using assembly bam $assembly" 1>&2
 fi
-output_vcf=$(readlink -f $output_vcf || echo -n)
-if [[ "$output_vcf" == "" ]] ; then
-	echo "$USAGE_MESSAGE"  1>&2
-	echo "Output VCF must be specified in a writable directory. Specify using the --output command line argument" 1>&2
-	exit 9
-fi 
-echo "Using output VCF $output_vcf" 1>&2
+
+##### --output
+if [[ $do_call == "true" ]] ; then
+	if [[ "$output_vcf" == "" ]] ; then
+		echo "$USAGE_MESSAGE"  1>&2
+		echo "Output VCF not specified. Use --output to specify output file." 1>&2
+		exit 9
+	fi
+	mkdir -p $(dirname $output_vcf) || echo "Unable to create directory $(dirname $output_vcf) for output VCF." 1>&2
+	echo "Using output VCF $output_vcf" 1>&2
+fi
+##### --threads
 if [[ "$threads" -lt 1 ]] ; then
 	echo "$USAGE_MESSAGE"  1>&2
 	echo "Illegal thread count: $threads. Specify an integer thread count using the --threads command line argument" 1>&2
@@ -164,44 +204,120 @@ elif [[ ! -f $blacklist ]] ; then
 	echo "Missing blacklist file $blacklist" 1>&2
 	exit 11
 else
-	blacklist_arg="BLACKLIST=$(readlink -f $blacklist)"
+	blacklist_arg="BLACKLIST=$blacklist"
 	echo "Using blacklist $blacklist" 1>&2
 fi
 if [[ "$jvmheap" == "" ]] ; then
-	if [[ $threads -gt 7 ]] ; then
-		jvmheap="31g"
-	else 
-		jvmheap="$((threads * 4))g"
+	if [[ $threads -gt 8 ]] ; then
+		echo "Warning: GRIDSS assembly may stall and run out of memory. with $threads and $jvmheap heap size." 1>&2
 	fi
 fi
-echo "Using JVM maximum heap size of $jvmheap" 1>&2
+echo "Using JVM maximum heap size of $jvmheap for assembly and variant calling." 1>&2
 if [[ "$@" == "" ]] ; then
 	echo "$USAGE_MESSAGE"  1>&2
 	echo "At least one input bam must be specified." 1>&2
 fi
-input_files=""
-for F in $@ ; do
-	F=$(readlink -f $F)
-	input_files="$input_files $F"
+for f in $@ ; do
+	if [[ ! -f $f ]] ; then
+		echo "Input file $f does not exist"  1>&2
+		exit 12
+	fi
 done
+config_args=""
+if [[ "$config_file" != "" ]] ; then
+	if [[ ! -f $config_file ]] ; then
+	echo "Configuration file $config_file does not exist"  1>&2
+		exit 13
+	fi
+	config_args="CONFIGURATION_FILE=$config_file"
+fi
 input_args=""
-for F in $input_files ; do
-	echo "Using input file $F" 1>&2
-	input_args="$input_args INPUT=$F"
+for f in $@ ; do
+	echo "Using input file $f" 1>&2
+	input_args="$input_args INPUT=$f"
 done
 
 # Validate tools exist on path
-if ! which bwa >/dev/null; then echo "Error: unable to find bwa on \$PATH" ; exit 2; fi
-if ! which java >/dev/null; then echo "Error: unable to find java on \$PATH" ; exit 2; fi
-if ! which Rscript >/dev/null; then echo "Error: unable to find Rscript on \$PATH" ; exit 2; fi
-if ! which /usr/bin/time >/dev/null; then echo "Error: unable to find /usr/bin/time" ; exit 2; fi
+for tool in bwa Rscript /usr/bin/time sambamba java ; do
+	if ! which $tool >/dev/null; then echo "Error: unable to find $tool on \$PATH" 1>&2 ; exit 2; fi
+	echo "Found $(which $tool)" 1>&2 
+done
 
+# check java version is ok by testing for GRIDSS usage message
+if java -cp $gridss_jar gridss.Echo 2>&1 | grep "USAGE:" >/dev/null ; then
+	java -version 2>&1
+else
+	echo "Unable to run GRIDSS jar. GRIDSS requires java 1.8 or later." 2>&1
+	java -version 2>&1
+	exit 14
+fi
+
+
+timestamp=$(date +%Y%m%d_%H%M%S)
 mkdir -p $workingdir
-logfile=$workingdir/gridss.full.$HOSTNAME.$$.log
-timinglogfile=$workingdir/gridss.timing.$HOSTNAME.$$.log
+logfile=$workingdir/gridss.full.$timestamp.$HOSTNAME.$$.log
+timinglogfile=$workingdir/gridss.timing.$timestamp.$HOSTNAME.$$.log
 
 ulimit -n $(ulimit -Hn) # Reduce likelihood of running out of open file handles 
 unset DISPLAY # Prevents errors attempting to connecting to an X server when starting the R plotting device
+echo "Max file handles: $(ulimit -n)" 1>&2 
+
+COMMON_JVM_ARGS="-Dreference_fasta=$reference \
+	-Dsamjdk.create_index=true \
+	-Dsamjdk.use_async_io_read_samtools=true \
+	-Dsamjdk.use_async_io_write_samtools=true \
+	-Dsamjdk.use_async_io_write_tribble=true \
+	-Dsamjdk.buffer_size=4194304 \
+	-Dgridss.gridss.output_to_temp_file=true"
+	
+for f in $@ ; do
+	echo "Pre-processing $F"
+	dir=$workingdir/$(basename $f).gridss.working
+	prefix=$workingdir/$(basename $f).gridss.working/$(basename $f)
+	tmp_prefix=$workingdir/$(basename $f).gridss.working/tmp.$(basename $f)
+	if ! mkdir -p $dir ; then
+		echo Unable to create directory $dir 1>&2
+		exit 2
+	fi
+	if [[ ! -f $prefix.sv.bam ]] ; then
+		echo "$(date)	Running	CollectInsertSizeMetrics	$f	$metricsrecords records" >> $timinglogfile
+		/usr/bin/time -a -o $timinglogfile java -Xmx256m $JVM_ARGS gridss.analysis.CollectGridssMetrics \
+			ASSUME_SORTED=true \
+			I=$f \
+			O=$tmp_prefix \
+			THRESHOLD_COVERAGE=$maxcoverage \
+			FILE_EXTENSION=null \
+			GRIDSS_PROGRAM=null \
+			PROGRAM=null \
+			PROGRAM=CollectInsertSizeMetrics \
+			STOP_AFTER=$metricsrecords 2>&1 | tee -a $logfile
+		exit
+		echo "$(date)	Running	CollectGridssMetricsAndExtractSVReads|sambamba	$f" >> $timinglogfile
+		/usr/bin/time -a -o $timinglogfile java -Xmx512m $JVM_ARGS gridss.analysis.CollectGridssMetricsAndExtractSVReads \
+			TMP_DIR=$gridss_dir \
+			ASSUME_SORTED=true \
+			I=$bam \
+			O=$prefix \
+			THRESHOLD_COVERAGE=$maxcoverage \
+			FILE_EXTENSION=null \
+			GRIDSS_PROGRAM=null \
+			GRIDSS_PROGRAM=CollectCigarMetrics \
+			GRIDSS_PROGRAM=CollectMapqMetrics \
+			GRIDSS_PROGRAM=CollectTagMetrics \
+			GRIDSS_PROGRAM=CollectIdsvMetrics \
+			GRIDSS_PROGRAM=ReportThresholdCoverage \
+			PROGRAM=null \
+			PROGRAM=CollectInsertSizeMetrics \
+			SV_OUTPUT=/dev/stdout \
+			COMPRESSION_LEVEL=0 \
+			METRICS_OUTPUT=$prefix.sv_metrics \
+			INSERT_SIZE_METRICS=$tmp_prefix \
+			UNMAPPED_READS=false \
+			MIN_CLIP_LENGTH=5 \
+			INCLUDE_DUPLICATES=true | \
+		sambamba sort --sort-picard --tmpdir $dir -m 8GB -o $tmp_prefix.coordinate.bam -t $threads - 2>&1 | tee -a $logfile
+	fi
+done
 
 # -Dreference_fasta is only required for CRAM input files
 # -Dgridss.gridss.output_to_temp_file=true allows GRIDSS to continue where it left off without data errors due to truncated files
@@ -209,13 +325,7 @@ unset DISPLAY # Prevents errors attempting to connecting to an X server when sta
 # -Dsamjdk.use_async_io allow for async read/write on background threads which improves BAM I/O performancce
 echo "gridss.analysis.CallVariants" >> $timinglogfile
 /usr/bin/time -a -o $timinglogfile java -Xmx$jvmheap \
-	-Dreference_fasta="$reference" \
-	-Dsamjdk.create_index=true \
-	-Dsamjdk.use_async_io_read_samtools=true \
-	-Dsamjdk.use_async_io_write_samtools=true \
-	-Dsamjdk.use_async_io_write_tribble=true \
-	-Dsamjdk.buffer_size=4194304 \
-	-Dgridss.gridss.output_to_temp_file=true \
+	 \
 	-cp $gridss_jar gridss.CallVariants \
 	TMP_DIR=$workingdir \
 	WORKING_DIR=$workingdir \
---
+++ src/main/java/gridss/Echo.java
@@ -0,0 +1,13 @@
+package gridss;
+
+/**
+ * Hello world entry point useful for testing whether the correct version of java is installed.
+ */
+public class Echo {
+	public static void main(String[] argv) {
+		for (String s : argv) {
+			System.out.println(s);
+		}
+        System.exit(0);
+    }
+}
