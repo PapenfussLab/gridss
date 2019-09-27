3cb6dd6d5e722641a438eb4da08d57300a80e280
Daniel Cameron <daniel.l.cameron@gmail.com>
2019-09-27 16:06:35 +1000

    #249 using breakpointgr2bedpe()

--- example/bedpe-export-example.R
+++ example/bedpe-export-example.R
@@ -6,21 +6,7 @@ vcf = readVcf("gridss.vcf")
 # Export breakpoints to BEDPE
 bpgr = breakpointRanges(vcf)
 # TODO: add your event filtering here. The default GRIDSS output is very verbose/sensitive.
-
-bpgr_first_breakend = bpgr[substring(names(bpgr), nchar(names(bpgr))) == "o"]
-bpgr_first_breakend_partner = bpgr[bpgr_first_breakend$partner]
-bedpedf = data.frame(
-	chrom1=seqnames(bpgr_first_breakend),
-	start1=start(bpgr_first_breakend),
-	end1=end(bpgr_first_breakend),
-	chrom2=seqnames(bpgr_first_breakend_partner),
-	start2=start(bpgr_first_breakend_partner),
-	end2=end(bpgr_first_breakend_partner),
-	name=names(bpgr_first_breakend),
-	score=bpgr_first_breakend$QUAL,
-	strand1=strand(bpgr_first_breakend),
-	strand2=strand(bpgr_first_breakend_partner))
-write.table(bedpedf, file="gridss_breakpoints.bedpe", sep="\t", quote=FALSE, col.names=FALSE)
+write.table(breakpointgr2bedpe(bpgr), file="gridss_breakpoints.bedpe", sep="\t", quote=FALSE, col.names=FALSE)
 	
 # Export single breakends to BED
 begr = breakendRanges(vcf)
