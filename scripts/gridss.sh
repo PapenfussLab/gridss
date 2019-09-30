441de4cfba79cf72557dbb0a75bb60dc652f8a09
Daniel Cameron <daniel.l.cameron@gmail.com>
2019-09-30 11:24:23 +1000

    #251 downgraded getopt failure to warning

--- scripts/gridss.sh
+++ scripts/gridss.sh
@@ -4,16 +4,16 @@
 #
 # Example ./gridss.sh  -t 4 -b wgEncodeDacMapabilityConsensusExcludable.bed -r ../hg19.fa -w out -o out/gridss.full.chr12.1527326.DEL1024.vcf -a out/gridss.full.chr12.1527326.DEL1024.assembly.bam -j ../target/gridss-2.6.2-gridss-jar-with-dependencies.jar --jvmheap 8g chr12.1527326.DEL1024.bam
 
+getopt --test
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+	echo 'WARNING: "getopt --test"` failed in this environment.' 1>&2
+	echo "WARNING: The version of getopt(1) installed on this system might not be compatible with the GRIDSS driver script." 1>&2
+fi
 set -o errexit -o pipefail -o noclobber -o nounset
 last_command=""
 current_command=""
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command completed with exit code $?."' EXIT
-! getopt --test > /dev/null 
-if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-    echo '`getopt --test` failed in this environment.'
-    exit 1
-fi
 #253 forcing C locale for everything
 export LC_ALL=C
 
