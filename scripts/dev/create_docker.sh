f751b92cab28df8134532b04197adffb1617e793
Daniel Cameron <d-cameron@github.com>
2020-05-12 23:00:33 +1000

    #340 Added packaging script to automate github release file set Added version sanity check on Dockerfile

--- scripts/dev/create_docker.sh
+++ scripts/dev/create_docker.sh
@@ -2,7 +2,12 @@
 #
 # GRIDSS docker container
 cd ../..
+pom_version=$(grep "gridss</version>" pom.xml | cut -f 2 -d '>' | cut -f 1 -d '-')
 version=$(grep GRIDSS_VERSION= Dockerfile | cut -b 20-)
+if [[ $pom_version != $version ]] ; then
+	echo Dockerfile GRIDSS version does not match pom.xml
+	exit 1
+fi
 echo Building GRIDSS $version
 docker build --tag gridss/gridss:latest .
 docker build --tag gridss/gridss:$version .
---
+++ scripts/dev/create_release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+cd ../../
+version=$(grep "gridss</version>" pom.xml | cut -f 2 -d '>' | cut -f 1 -d '-')
+echo Packaging GRIDSS $version
+mvn clean package
+mkdir target/github_package
+cp scripts/*.sh target/github_package/
+cp scripts/*.R target/github_package/
+cp target/gridss-$version-gridss-jar-with-dependencies.jar target/github_package/
+cd target/github_package
+tar -czvf gridss-$version.tar.gz *
