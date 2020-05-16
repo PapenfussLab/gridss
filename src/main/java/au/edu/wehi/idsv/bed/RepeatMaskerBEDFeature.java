569372edc74003a8981dd27ba5e95607c218230c
Daniel Cameron <d-cameron@github.com>
2020-05-16 16:13:17 +1000

    Reduced RepeatMaskerBEDFeature memory usage

--- src/main/java/au/edu/wehi/idsv/bed/RepeatMaskerBEDFeature.java
+++ src/main/java/au/edu/wehi/idsv/bed/RepeatMaskerBEDFeature.java
@@ -5,81 +5,87 @@ import htsjdk.tribble.bed.BEDFeature;
 import htsjdk.tribble.bed.FullBEDFeature;
 
 import java.awt.*;
+import java.util.Collections;
 import java.util.List;
 
+/**
+ * See https://bedops.readthedocs.io/en/latest/content/reference/file-management/conversion/rmsk2bed.html#column-mapping for format
+ */
 public class RepeatMaskerBEDFeature implements BEDFeature {
-    private final BEDFeature feature;
-    private final String[] tokens;
+    private final String contig;
+    private final int start;
+    private final int end;
+    private final String repeatType;
+    private final double swScore;
+    private final Strand strand;
+    private final String repeatClass;
+
+    public RepeatMaskerBEDFeature(BEDFeature feature, String[] tokens) {
+        this.start = feature.getStart();
+        this.end = feature.getEnd();
+        this.strand = feature.getStrand();
+        this.swScore = feature.getScore();
+        this.repeatType = feature.getName();
+        this.contig = feature.getContig();
+        this.repeatClass = tokens[10];
+    }
 
     public String getRepeatType() {
-        return getName();
+        return repeatType;
     }
 
     public int getSmithWatermanScore() {
-        return (int)getScore();
+        return (int)swScore;
     }
 
     public String getRepeatClass() {
-        return tokens[10];
-    }
-
-    public RepeatMaskerBEDFeature(BEDFeature feature, String[] tokens) {
-        this.feature = feature;
-        this.tokens = tokens;
+        return repeatClass;
     }
 
     @Override
     public Strand getStrand() {
-        return feature.getStrand();
+        return strand;
     }
 
     @Override
     public String getType() {
-        return feature.getType();
+        return getRepeatType();
     }
 
     @Override
-    public Color getColor() {
-        return feature.getColor();
-    }
+    public Color getColor() { return null; }
 
     @Override
-    public String getDescription() {
-        return feature.getDescription();
-    }
+    public String getDescription() { return null; }
 
     @Override
-    public List<FullBEDFeature.Exon> getExons() {
-        return feature.getExons();
-    }
+    public List<FullBEDFeature.Exon> getExons()  { return null; }
 
     @Override
     public String getName() {
-        return feature.getName();
+        return getRepeatType();
     }
 
     @Override
     public float getScore() {
-        return feature.getScore();
+        return getSmithWatermanScore();
     }
 
     @Override
-    public String getLink() {
-        return feature.getLink();
-    }
+    public String getLink() { return null; }
 
     @Override
     public String getContig() {
-        return feature.getContig();
+        return contig;
     }
 
     @Override
     public int getStart() {
-        return feature.getStart();
+        return start;
     }
 
     @Override
     public int getEnd() {
-        return feature.getEnd();
+        return end;
     }
 }
