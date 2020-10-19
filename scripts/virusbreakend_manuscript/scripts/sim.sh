9557027090c5224bced2ef0ad917f82783d10b3b
Daniel Cameron <d-cameron@github.com>
2020-10-19 21:11:36 +1100

    Added virusbreakend simulation

---
+++ scripts/virusbreakend_manuscript/scripts/sim.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+export PATH=~/dev/gridss/:$PATH
+export GRIDSS_JAR=~/projects/virusbreakend/gridss-2.10.1-gridss-jar-with-dependencies.jar
+hpv=MT783410.1
+hbv=LC500247.1
+chr1_len=248387497
+n=0
+pos=0
+flank=50000
+breakoffset=10
+virus_lengh=1000
+readlen=150
+virusbreakenddb=~/dev/virusbreakend/virusbreakenddb
+ref=~/dev/ref/hg19.fa
+if [[ ! -f gen/chm13.bam.gridss.working/chm13.bam.insert_size_metrics ]] ; then
+	# GRIDSS uses WGS metrics which are going to be wrong for the small simulation - create a baseline
+	wgsim -r 0 -R 0 -X 0 -N 20000000 -1 $readlen -2 $readlen chm13.draft_v1.0.fasta gen/chm13.1.fq gen/chm13.2.fq
+	bwa mem -t $(nproc) $ref gen/chm13.1.fq gen/chm13.2.fq | samtools sort -@ $(nproc) -o gen/chm13.bam -O BAM -
+	gridss.sh -r $ref -s preprocess gen/chm13.bam -w gen/
+fi
+rm -f gen/wgsim.sh
+while [[ $n -lt 248 ]] ; do
+	n=$(($n + 1))
+	pos=$(($n * 1000000))
+	virus_pos=$(($n * 8))
+	file=gen/chr1_${pos}.fa
+	if [[ ! -f $file ]] ; then
+		echo ">$file" >> $file
+		samtools faidx chm13.draft_v1.0.fasta chr1:$(($pos - $flank))-$(($pos)) | grep -v ">" | tr -d '\n' >> $file
+		samtools faidx $hbv.fa $hbv:$virus_pos-$(($virus_pos + $virus_lengh)) | grep -v ">" | tr -d '\n' >> $file
+		samtools faidx chm13.draft_v1.0.fasta chr1:$(($pos + $breakoffset))-$(($pos + $flank + $breakoffset)) | grep -v ">" | tr -d '\n' >> $file
+		echo "Generated $n"
+	fi
+	reflen=$(( 2 * $flank + $virus_lengh ))
+	pairbases=$(( 2 * $readlen ))
+	for depth in 5 10 15 30 60 ; do
+		if [[ ! -f $file.${depth}x.1.fq ]] ; then
+			wgsim -r 0 -R 0 -X 0 -N $(( $depth * $reflen / $pairbases )) -1 150 -2 150 $file $file.${depth}x.1.fq $file.${depth}x.2.fq
+		fi
+		vcf=gen/virusbreakend_${n}_${depth}x.vcf
+		if [[ ! -f $vcf.virusbreakend.working/$(basename $vcf).kraken2.report.viral.extracted.txt ]] ; then
+			bam=$file.${depth}x.bam
+			metrics_prefix=$vcf.virusbreakend.working/adjusted/$(basename $bam).viral.bam.gridss.working/$(basename $bam).viral.bam
+			mkdir -p $(dirname $metrics_prefix)
+			for metric_suffix in cigar_metrics idsv_metrics insert_size_metrics mapq_metrics tag_metrics ; do
+				if [[ ! -f $metrics_prefix.$metric_suffix ]] ; then
+					cp gen/chm13.bam.gridss.working/chm13.bam.$metric_suffix $metrics_prefix.$metric_suffix
+				fi
+			done
+cat > gen/slurm_virusbreakend_${n}_${depth}x.sh << EOF
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16g
+#SBATCH --time=12:00:00
+#SBATCH -p regular
+#SBATCH --output=log.virusbreakend.%x.out
+#SBATCH --error=log.virusbreakend.%x.err
+export PATH=~/projects/virusbreakend/gridss/scripts:$PATH
+if [[ ! -f $bam ]] ; then
+	echo "Creating $bam"
+	bwa mem -t 4 $ref $file.${depth}x.1.fq $file.${depth}x.2.fq | samtools sort -@ 4 -o $bam -O BAM -
+	samtools index $bam
+fi
+virusbreakend.sh --db $virusbreakenddb -r $ref -t 4 -o $vcf -j $GRIDSS_JAR --rmargs "-e rmblast" -w gen/ $bam
+EOF
+		fi
+		mkdir -p gen/batvi_${n}_${depth}x
+		if [[ ! -f gen/batvi_${n}_${depth}x/filelist.txt ]] ; then
+			echo "$file.${depth}x.1.fq;$file.${depth}x.1.fq;500" > gen/batvi_${n}_${depth}x/filelist.txt
+			echo > gen/batvi_${n}_${depth}x/batvirun.log.placeholder
+		fi
+		cat > gen/slurm_batvi_${n}_${depth}x.sh << EOF
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16g
+#SBATCH --time=12:00:00
+#SBATCH -p regular
+#SBATCH --output=log.batvi.%x.out
+#SBATCH --error=log.batvi.%x.err
+. ~/conda_crest/etc/profile.d/conda.sh
+conda activate virusbreakend
+cd $PWD
+cd batvi_${n}_${depth}x
+echo > batvirun.log.placeholder
+~/projects/virusbreakend/batvi/batvi1.03/call_integrations.sh $PWD/batvi/batvi_${n}_${depth}x
+EOF
+	done
+done
